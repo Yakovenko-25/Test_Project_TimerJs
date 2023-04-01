dfe9530cf6963b52788ea352259ca9eb3ff61a36
Denis Yakovenko <kn202_yadv@student.ztu.edu.ua>
2023-04-01 19:13:55 +0300

    Персональное время для таймера
    Добавил самостоятельное изменение времени + при [reset] время возращается к тому что задал пользователь

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import { useState, useEffect } from "react";
 import { getPadTime } from "./helpers/getPadTime";
-import "./App.css";
+import "./style.css";
 
 function App() {
-  const [timeLeft, setTimeLeft] = useState(5);
+  const [timeLeft, setTimeLeft] = useState(120);
   const [isCounting, setIsCounting] = useState(false);
+  const [inputMinutes, setInputMinutes] = useState(120);
 
   const minutes = getPadTime(Math.floor(timeLeft / 60));
   const seconds = getPadTime(timeLeft - minutes * 60);
@@ -15,7 +16,7 @@ function App() {
         setTimeLeft((timeLeft) => (timeLeft >= 1 ? timeLeft - 1 : 0));
     }, 1000);
 
-    if (timeLeft == 0) setIsCounting(false);
+    if (timeLeft === 0) setIsCounting(false);
 
     return () => {
       clearInterval(interval);
@@ -23,7 +24,7 @@ function App() {
   }, [timeLeft, isCounting]);
 
   const handleStart = () => {
-    if (timeLeft == 0) setTimeLeft(5);
+    if (timeLeft === 0) setTimeLeft(5);
 
     setIsCounting(true);
   };
@@ -34,11 +35,22 @@ function App() {
 
   const handleReset = () => {
     setIsCounting(false);
-    setTimeLeft(5);
+    setTimeLeft(inputMinutes);
   };
+
+  function setMinutes(event) {
+    setTimeLeft(event.target.value);
+    setInputMinutes(event.target.value);
+  }
 
   return (
     <div className="app">
+      <section className="inputValue">
+        <div className="minutes">
+          <p>Set time (in seconds)</p>
+          <input type="text" onInput={setMinutes} />
+        </div>
+      </section>
       <div className="timer">
         <span>{minutes}</span>
         <span>:</span>
