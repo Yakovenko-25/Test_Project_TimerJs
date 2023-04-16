792e993a4b850407599480fb72c2066706383057
DenisYakovenko <kn202_yadv@student.ztu.edu.ua>
2023-04-16 12:40:36 +0300

    Finish revert style in inputValue.jsx

--- src/components/InputValue/inputValue.jsx
+++ src/components/InputValue/inputValue.jsx
@@ -1,32 +1,52 @@
 import React from "react";
 import styled from "styled-components";
 
-const Input = styled.section`
-
+const InputBox = styled.section`
+  & p {
+    font-size: 35px;
+    margin: 0;
+    color: red;
+  }
 `;
 
 const Minutes = styled.div`
     display: flex;
-    background-color: black;
     flex-direction: column;
-    & p{
-      text-size: 50px;
+    & input[type=text] {
+      padding: 10px;
+      align-self: flex-end;
+      width: 35%;
+      background: transparent;
+      border: 1px solid #000;
+      border-bottom-right-radius: 10px;
+      border-top-left-radius: 10px;
+      font-size: 20px;
+      &:focus {
+        background-color: #A61F12;
+        color: white;
+        border: 1px solid transparent;
+        border-top-left-radius: 10px;
+        outline-style: none;
+        &::-moz-placeholder {
+          color: transparent;
+        }
+        &::placeholder {
+          color: transparent;
+        }
+      }
     }
+    
 `; 
-
-const inputText = styled.input`
-
-`
 
 
 const InputValue = ({setMinutes}) => {
   return (
-    <Input>
+    <InputBox>
       <Minutes>
         <p>Set time (in seconds)</p>
         <input type="text" onInput={setMinutes} placeholder="seconds"/>
       </Minutes>
-    </Input>
+    </InputBox>
   );
 };
 
